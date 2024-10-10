84f3dbf9591a0d4d726f7d7eb65e9d586ab71240
Luis Phillip Lemos Martins <107713925+Gaok1@users.noreply.github.com>
2024-10-10 19:51:37 -0300

    aeds2 in Rust

---
+++ src/bin/P_binaria/main.rs
@@ -0,0 +1,241 @@
+use std::{
+    fmt,
+    fs::File,
+    io::{BufReader, Read},
+};
+
+const POKEMON_LEN: usize = 802;
+
+/*
+weight double, height double, captureRate int, isLegendary boolean, captureDate Date.
+ */
+#[derive(Debug,Clone)]
+struct Date {
+    day: i32,
+    month: i32,
+    year: i32,
+}
+impl Date {
+    fn from_str(str: &str) -> Date {
+        let date: Vec<&str> = str.split("/").collect();
+        Date {
+            day: date[0].parse::<i32>().unwrap(),
+            month: date[1].parse::<i32>().unwrap(),
+            year: date[2].parse::<i32>().unwrap(),
+        }
+    }
+}
+#[derive(Debug,Clone)]
+struct Pokemon {
+    id: i32,
+    generation: i32,
+    name: String,
+    description: String,
+    types: Vec<String>,
+    abilities: Vec<String>,
+    weight: f64,
+    height: f64,
+    capture_rate: i32,
+    is_legendary: bool,
+    capture_date: Date,
+}
+
+impl Pokemon {
+    fn extract_habilities(&mut self, csv_line: &str) -> String {
+        let idx = csv_line.find('[').unwrap();
+        let final_idx = csv_line.find(']').unwrap();
+        let mut new_line = csv_line[..idx].to_string();
+        new_line.push_str(&csv_line[final_idx + 1..]);
+
+        let habilitiy_str = csv_line[idx + 1..final_idx - 1].replace("'", "");
+        let habilitiy_str = habilitiy_str.split(",");
+        for hab in habilitiy_str {
+            if hab == "" {
+                continue;
+            }
+            self.abilities.push(hab.trim().to_string());
+        }
+
+        new_line
+    }
+
+    pub fn new() -> Pokemon {
+        Pokemon {
+            id: 0,
+            generation: 0,
+            name: "".to_string(),
+            description: "".to_string(),
+            types: Vec::new(),
+            abilities: Vec::new(),
+            weight: 0.0,
+            height: 0.0,
+            capture_rate: 0,
+            is_legendary: false,
+            capture_date: Date {
+                day: 0,
+                month: 0,
+                year: 0,
+            },
+        }
+    }
+
+    pub fn from_str(line: &str) -> Pokemon {
+        let mut pokemon = Pokemon::new();
+        let cleared_line = pokemon.extract_habilities(line);
+
+        let atributes: Vec<&str> = cleared_line.split(",").collect();
+        pokemon.id = atributes[0].parse().unwrap();
+        pokemon.generation = atributes[1].parse().unwrap();
+        pokemon.name = atributes[2].to_string();
+        pokemon.description = atributes[3].to_string();
+        pokemon.types.push(atributes[4].to_string());
+        if atributes[5].len() > 1 {
+            pokemon.types.push(atributes[5].to_string());
+        }
+        if atributes[7].len() > 1 {
+            pokemon.weight = atributes[7].parse().unwrap();
+        } else {
+            pokemon.weight = 0.0;
+        }
+        if atributes[8].len() > 1 {
+            pokemon.height = atributes[8].parse().unwrap();
+        } else {
+            pokemon.height = 0.0;
+        }
+        if atributes[9].len() > 1 {
+            pokemon.capture_rate = atributes[9].parse().unwrap();
+        } else {
+            pokemon.capture_rate = 0;
+        }
+        pokemon.is_legendary = atributes[10] == "1";
+        if atributes[11].len() > 1 {
+            pokemon.capture_date = Date::from_str(atributes[11]);
+        }
+
+        pokemon
+    }
+
+    pub fn vec_from_file(file: File) -> Vec<Pokemon> {
+        let mut reader = BufReader::new(file);
+        let mut content = String::new();
+        reader
+            .read_to_string(&mut content)
+            .expect("Failed to read file");
+
+        let mut pokemons: Vec<Pokemon> = Vec::with_capacity(POKEMON_LEN);
+        let mut lines = content.split("\n");
+        lines.next();
+
+        for csv_line in lines {
+            pokemons.push(Pokemon::from_str(csv_line));
+        }
+
+        pokemons
+    }
+}
+
+impl fmt::Display for Pokemon {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Format types and abilities to match the style of the example in Java
+        let types_str = self.types.iter().map(|t| format!("'{}'", t)).collect::<Vec<_>>().join(", ");
+        let abilities_str = self.abilities.iter().map(|a| format!("'{}'", a)).collect::<Vec<_>>().join(", ");
+
+        // Add leading zero to day and month if necessary
+        let day = format!("{:02}", self.capture_date.day);
+        let month = format!("{:02}", self.capture_date.month);
+
+        write!(
+            f,
+            "[#{} -> {}: {} - [{}] - [{}] - {:.1}kg - {:.1}m - {}% - {} - {} gen] - {}/{}/{}",
+            self.id,
+            self.name,
+            self.description,
+            types_str,
+            abilities_str,
+            self.weight,
+            self.height,
+            self.capture_rate,
+            self.is_legendary,
+            self.generation,
+            day,
+            month,
+            self.capture_date.year
+        )   
+    }
+}
+
+// impl Drop for Pokemon{
+//     fn drop(&mut self) {
+//         println!("Dropping Pokemon: {}", self.name);
+//     }
+// }
+
+
+fn main() {
+    let file: File = File::open("pokemon.csv").expect("Arquivo não encontrado!");
+
+    let mut pokemons = Pokemon::vec_from_file(file);
+    let mut entrada = String::new();
+
+    let mut use_pokemons : Vec<Pokemon> = Vec::new();
+    text_io::scan!("{}", entrada);
+    //coletando primeira entrada
+    while entrada.trim() != "FIM" {
+        let id = entrada.parse::<usize>();
+        use_pokemons.push(pokemons[id.unwrap()-1].clone());
+        text_io::scan!("{}", entrada);
+    }
+    // reutilizando variavel
+    let mut pokemons = use_pokemons;
+    
+    sort(&mut pokemons); //ordenando
+
+    text_io::scan!("{}", entrada);
+    //pesqusar os nomes por pesqusia binária
+    while entrada.trim() != "FIM" {
+        let name = entrada.trim();
+        if bin_search(name, &pokemons) {
+            println!("SIM");
+        } else {
+            println!("NAO");
+        }
+        text_io::scan!("{}", entrada);
+    }
+}
+
+//simples selection Sort
+fn sort(array: &mut Vec<Pokemon>) {
+    for i in 0..array.len() {
+        let mut min = i;
+        for j in i + 1..array.len() {
+            if array[j].name < array[min].name {
+                min = j;
+            }
+        }
+        if i != min {
+            // Usa std::mem::swap para trocar os elementos
+            array.swap(i, min);
+        }
+    }
+}
+
+
+fn bin_search(name: &str, pokemons: &Vec<Pokemon>) -> bool {
+    let mut left = 0;
+    let mut right = pokemons.len() -1 ;
+
+    while left <= right {
+        let index = (left + right)/2;
+        if pokemons[index].name == name{
+            return true;
+        }
+        if name < &pokemons[index].name { //borrow por que o name é uma referencia e tem q ter mesmo tipo para fazer comparação
+            //referencia compara com  referencia 
+            right = index - 1;
+        }else{
+            left = index + 1;
+        }
+    }
+
+    false
+}
---
+++ src/bin/shellSort/main.rs
@@ -0,0 +1,223 @@
+use std::{
+    cmp::Ordering, fmt, fs::File, io::{BufReader, Read}
+};
+
+const POKEMON_LEN: usize = 802;
+
+/*
+weight double, height double, captureRate int, isLegendary boolean, captureDate Date.
+ */
+#[derive(Debug,Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct Date {
+    year: i32,
+    month: i32,
+    day: i32,
+}
+impl Date {
+    fn from_str(str: &str) -> Date {
+        let date: Vec<&str> = str.split("/").collect();
+        Date {
+            year: date[2].parse::<i32>().unwrap(),
+            month: date[1].parse::<i32>().unwrap(),
+            day: date[0].parse::<i32>().unwrap(),
+        }
+    }
+}
+
+
+#[derive(Debug,Clone)]
+struct Pokemon {
+    id: i32,
+    generation: i32,
+    name: String,
+    description: String,
+    types: Vec<String>,
+    abilities: Vec<String>,
+    weight: f64,
+    height: f64,
+    capture_rate: i32,
+    is_legendary: bool,
+    capture_date: Date,
+}
+
+impl Pokemon {
+    fn extract_habilities(&mut self, csv_line: &str) -> String {
+        let idx = csv_line.find('[').unwrap();
+        let final_idx = csv_line.find(']').unwrap();
+        let mut new_line = csv_line[..idx].to_string();
+        new_line.push_str(&csv_line[final_idx + 1..]);
+
+        let habilitiy_str = csv_line[idx + 1..final_idx - 1].replace("'", "");
+        let habilitiy_str = habilitiy_str.split(",");
+        for hab in habilitiy_str {
+            if hab == "" {
+                continue;
+            }
+            self.abilities.push(hab.trim().to_string());
+        }
+
+        new_line
+    }
+
+    pub fn new() -> Pokemon {
+        Pokemon {
+            id: 0,
+            generation: 0,
+            name: "".to_string(),
+            description: "".to_string(),
+            types: Vec::new(),
+            abilities: Vec::new(),
+            weight: 0.0,
+            height: 0.0,
+            capture_rate: 0,
+            is_legendary: false,
+            capture_date: Date {
+                day: 0,
+                month: 0,
+                year: 0,
+            },
+        }
+    }
+
+    pub fn from_str(line: &str) -> Pokemon {
+        let mut pokemon = Pokemon::new();
+        let cleared_line = pokemon.extract_habilities(line);
+
+        let atributes: Vec<&str> = cleared_line.split(",").collect();
+        pokemon.id = atributes[0].parse().unwrap();
+        pokemon.generation = atributes[1].parse().unwrap();
+        pokemon.name = atributes[2].to_string();
+        pokemon.description = atributes[3].to_string();
+        pokemon.types.push(atributes[4].to_string());
+        if atributes[5].len() > 1 {
+            pokemon.types.push(atributes[5].to_string());
+        }
+        if atributes[7].len() > 1 {
+            pokemon.weight = atributes[7].parse().unwrap();
+        } else {
+            pokemon.weight = 0.0;
+        }
+        if atributes[8].len() > 1 {
+            pokemon.height = atributes[8].parse().unwrap();
+        } else {
+            pokemon.height = 0.0;
+        }
+        if atributes[9].len() > 1 {
+            pokemon.capture_rate = atributes[9].parse().unwrap();
+        } else {
+            pokemon.capture_rate = 0;
+        }
+        pokemon.is_legendary = atributes[10] == "1";
+        if atributes[11].len() > 1 {
+            pokemon.capture_date = Date::from_str(atributes[11]);
+        }
+
+        pokemon
+    }
+
+    pub fn vec_from_file(file: File) -> Vec<Pokemon> {
+        let mut reader = BufReader::new(file);
+        let mut content = String::new();
+        reader
+            .read_to_string(&mut content)
+            .expect("Failed to read file");
+
+        let mut pokemons: Vec<Pokemon> = Vec::with_capacity(POKEMON_LEN);
+        let mut lines = content.split("\n");
+        lines.next();
+
+        for csv_line in lines {
+            pokemons.push(Pokemon::from_str(csv_line));
+        }
+
+        pokemons
+    }
+}
+
+impl fmt::Display for Pokemon {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Format types and abilities to match the style of the example in Java
+        let types_str = self.types.iter().map(|t| format!("'{}'", t)).collect::<Vec<_>>().join(", ");
+        let abilities_str = self.abilities.iter().map(|a| format!("'{}'", a)).collect::<Vec<_>>().join(", ");
+
+        // Add leading zero to day and month if necessary
+        let day = format!("{:02}", self.capture_date.day);
+        let month = format!("{:02}", self.capture_date.month);
+
+        write!(
+            f,
+            "[#{} -> {}: {} - [{}] - [{}] - {:.1}kg - {:.1}m - {}% - {} - {} gen] - {}/{}/{}",
+            self.id,
+            self.name,
+            self.description,
+            types_str,
+            abilities_str,
+            self.weight,
+            self.height,
+            self.capture_rate,
+            self.is_legendary,
+            self.generation,
+            day,
+            month,
+            self.capture_date.year
+        )   
+    }
+}
+
+// impl Drop for Pokemon{
+//     fn drop(&mut self) {
+//         println!("Dropping Pokemon: {}", self.name);
+//     }
+// }
+
+
+fn main() {
+    let file: File = File::open("pokemon.csv").expect("Arquivo não encontrado!");
+
+    let mut pokemons = Pokemon::vec_from_file(file);
+    let mut entrada = String::new();
+
+    let mut use_pokemons : Vec<Pokemon> = Vec::new();
+    text_io::scan!("{}", entrada);
+    //coletando primeira entrada
+    while entrada.trim() != "FIM" {
+        let id = entrada.parse::<usize>();
+        use_pokemons.push(pokemons[id.unwrap()-1].clone());
+        text_io::scan!("{}", entrada);
+    }
+    // reutilizando variavel
+    let mut pokemons = use_pokemons;
+    
+    shell_sort(&mut pokemons); //ordenando
+
+    for pokemon in pokemons {
+        println!("{}", pokemon);
+    }
+    
+}
+fn shell_sort(array: &mut Vec<Pokemon>) {
+    let len = array.len();
+    let mut gap = len / 2;
+
+    while gap > 0 {
+        for i in gap..len {
+           
+            let val = array[i].clone();
+            let mut j = i;
+
+            // Mova os elementos que estão gap posições atrás para frente
+            while j >= gap && array[j - gap].weight > val.weight {
+                array[j] = array[j - gap].clone(); // Move o elemento
+                j -= gap;
+            }
+
+            // Insere o valor na posição correta
+            array[j] = val;
+        }
+
+        // Reduz o gap
+        gap /= 2;
+    }
+}
+
+
